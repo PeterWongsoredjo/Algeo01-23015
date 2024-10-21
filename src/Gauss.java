780ece8cd447efad1e7f7b7094105dafe5564793
PeterWongsoredjo <19623284@std.stei.itb.ac.id>
2024-10-21 12:43:05 +0700

    fix: SPL

--- src/Gauss.java
+++ src/Gauss.java
@@ -1,4 +1,8 @@
+import java.math.*;
+
 public class Gauss{
+    private static double EPSILON = 1e-10;
+
     public void gauss(Matrix M){
         Matrix temp = new Matrix();
         temp = M;
@@ -14,22 +18,29 @@ public class Gauss{
 
         for (i = 0; i < x+1; i++){
             // Memastikan diagonal bukan 0
-            if(M.getElement(i, i) == 0){
+            if(Math.abs(M.getElement(i, i)) < EPSILON){
                 for(int j = i; j<= M.getLastRowIdx(temp); j++){
-                    if (M.getElement(j, i) != 0){
+                    if (Math.abs(M.getElement(j, i)) >= EPSILON){
                         GJ.switchRows(temp, i, j);
                         break;
                     }
                 }
             }
 
-            if (M.getElement(i, i) != 0){
+            if (Math.abs(M.getElement(i, i)) >= EPSILON){
                 // Membagi baris diagonal dengan diagonal sendiri (mendapatkan 1)
                 GJ.divideByX(temp, i, M.getElement(i, i));
 
                 // Mengurangkan baris bawah dengan multiplier diagonal
                 for (int j = i+1; j <= M.getLastRowIdx(temp); j++){
                     GJ.plusMinRows(temp, j, i, GJ.multiplier(temp, i, j));
+                }
+            }
+        }
+        for (int row = 0; row <= M.getLastRowIdx(temp); row++) {
+            for (int col = 0; col <= M.getLastColIdx(temp); col++) {
+                if (Math.abs(M.getElement(row, col)) < EPSILON) {
+                    temp.setElement(temp, row, col, 0); 
                 }
             }
         }
