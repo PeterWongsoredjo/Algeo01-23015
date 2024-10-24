f0f5b72754f0dcec84831a30c3d8bdeb8db62a07
KaindraMaheswara <kaindramaheswara11@gmail.com>
2024-10-24 12:52:50 +0700

    feat: fixing bugs on SPLCramer

--- src/SPLCramer.java
+++ src/SPLCramer.java
@@ -16,93 +16,98 @@ public class SPLCramer extends Matrix {
     DeterminanKofaktor det = new DeterminanKofaktor();
     double detM = det.detkof(M1);
 
+    if (M1.getCol(M1) == M1.getRow(M1)){
     // Hitung Hasil
-    Matrix mHasil = new Matrix();
-    CreateMatrix(mHasil, getRow(M1), 1);
-    Matrix tempcheck = new Matrix();
-    int count_null = 0;
+      Matrix mHasil = new Matrix();
+      CreateMatrix(mHasil, getRow(M1), 1);
+      Matrix tempcheck = new Matrix();
+      int count_null = 0;
 
-    GaussJordan GJ = new GaussJordan();
-    tempcheck.CreateMatrix(tempcheck, M1.getRow(M1), M1.getCol(M1) + 1);
+      GaussJordan GJ = new GaussJordan();
+      tempcheck.CreateMatrix(tempcheck, M1.getRow(M1), M1.getCol(M1) + 1);
 
-    for(int i = 0; i<M1.getRow(M1); i++){
-      for(int j = 0; j<M1.getCol(M1); j++){
-        tempcheck.setElement(tempcheck, i, j, M1.getElement(i, j));
-      }
-    }
-
-    for(int i = 0; i < M1.getRow(M1); i++){
-      tempcheck.setElement(tempcheck, i, tempcheck.getLastColIdx(tempcheck), M2.getElement(i, 0));
-    }
-
-    GJ.gaussjordan(tempcheck);
-    for(int i = tempcheck.getLastRowIdx(tempcheck); i>=0; i--){    
-      boolean temp_kosong = true;
-      for(int j=0; j < tempcheck.getLastColIdx(tempcheck); j++){
-        // Cek Kosong
-        if (tempcheck.getElement(i, j) != 0){
-          temp_kosong =false;
+      for(int i = 0; i<M1.getRow(M1); i++){
+        for(int j = 0; j<M1.getCol(M1); j++){
+          tempcheck.setElement(tempcheck, i, j, M1.getElement(i, j));
         }
       }
-      if(temp_kosong){
-        if(tempcheck.getElement(i, tempcheck.getLastColIdx(tempcheck)) == 0){
-          kosong = true;
-          count_null ++;
+
+      for(int i = 0; i < M1.getRow(M1); i++){
+        tempcheck.setElement(tempcheck, i, tempcheck.getLastColIdx(tempcheck), M2.getElement(i, 0));
+      }
+
+      GJ.gaussjordan(tempcheck);
+      for(int i = tempcheck.getLastRowIdx(tempcheck); i>=0; i--){    
+        boolean temp_kosong = true;
+        for(int j=0; j < tempcheck.getLastColIdx(tempcheck); j++){
+          // Cek Kosong
+          if (tempcheck.getElement(i, j) != 0){
+            temp_kosong =false;
+          }
         }
-        if(tempcheck.getElement(i, tempcheck.getLastColIdx(tempcheck)) != 0){
-          no_solution = true;
+        if(temp_kosong){
+          if(tempcheck.getElement(i, tempcheck.getLastColIdx(tempcheck)) == 0){
+            kosong = true;
+            count_null ++;
+          }
+          if(tempcheck.getElement(i, tempcheck.getLastColIdx(tempcheck)) != 0){
+            no_solution = true;
+          }
         }
       }
-    }
 
-    if(no_solution || M1.getRow(M1) != M1.getCol(M1)){
-      result.append("Tidak ada solusi yang memenuhi.");
-    }
+      if(no_solution){
+        result.append("Tidak ada solusi yang memenuhi.");
+      }
 
-    if(!no_solution && (tempcheck.getCol(tempcheck) + count_null -1) > tempcheck.getRow(tempcheck)){
-      for(int i = 0; i<=tempcheck.getLastRowIdx(tempcheck); i++){
-        int first = 0; 
-        if (!kosong(tempcheck, i)){ 
-          for(int j = 0; j<tempcheck.getCol(tempcheck) - 1; j++){          
-            if (first == 0){
-              if (tempcheck.getElement(i, j) != 0){
-                result.append (tempcheck.getElement(i, j) + " x" + (j+1) + " ");
-                first = 1;
-              }
-            } 
-            else{
-              if(tempcheck.getElement(i, j) > 0){
-                result.append("+" + tempcheck.getElement(i, j) + " x" + (j+1) + " ");
-              }
-              if(tempcheck.getElement(i, j) < 0){
-                result.append(tempcheck.getElement(i, j) + " x" + (j+1) + " ");
+      if(!no_solution && (tempcheck.getCol(tempcheck) + count_null -1) > tempcheck.getRow(tempcheck)){
+        for(int i = 0; i<=tempcheck.getLastRowIdx(tempcheck); i++){
+          int first = 0; 
+          if (!kosong(tempcheck, i)){ 
+            for(int j = 0; j<tempcheck.getCol(tempcheck) - 1; j++){          
+              if (first == 0){
+                if (tempcheck.getElement(i, j) != 0){
+                  result.append (tempcheck.getElement(i, j) + " x" + (j+1) + " ");
+                  first = 1;
+                }
+              } 
+              else{
+                if(tempcheck.getElement(i, j) > 0){
+                  result.append("+" + tempcheck.getElement(i, j) + " x" + (j+1) + " ");
+                }
+                if(tempcheck.getElement(i, j) < 0){
+                  result.append(tempcheck.getElement(i, j) + " x" + (j+1) + " ");
+                }
               }
             }
+            result.append("= " + tempcheck.getElement(i, tempcheck.getCol(tempcheck) - 1));
+            result.append("\n");
+          }   
+        }
+      }
+      
+      if (det.detkof(M1) != 0){
+        Matrix temp = new Matrix();
+        for (int j = 0; j < temp.getCol(M1); j++){
+          CreateMatrix(temp, getRow(M1), getCol(M1));
+          copyMatrix(M1, temp);
+          System.out.println("X\n");
+          for (int i = 0; i < temp.getRow(temp); i++){
+            temp.setElement(temp, i, j, M2.getElement(i, 0));
           }
-          result.append("= " + tempcheck.getElement(i, tempcheck.getCol(tempcheck) - 1));
-          result.append("\n");
-        }   
+          printMatrix(temp);
+          double newDet = det.detkof(temp);
+          System.out.println(newDet + " " + detM);
+          double resultDet = newDet / detM;
+          mHasil.setElement(mHasil, j, 0, resultDet);
+        }
+        for(int i = 0; i<=mHasil.getLastRowIdx(mHasil) ; i++){
+          result.append("x" + (i+1) + " = " + mHasil.getElement(i, 0) + "\n");
+        } 
       }
     }
-    
-    if (det.detkof(M1) != 0){
-      Matrix temp = new Matrix();
-      for (int j = 0; j < temp.getCol(M1); j++){
-        CreateMatrix(temp, getRow(M1), getCol(M1));
-        copyMatrix(M1, temp);
-        System.out.println("X\n");
-        for (int i = 0; i < temp.getRow(temp); i++){
-          temp.setElement(temp, i, j, M2.getElement(i, 0));
-        }
-        printMatrix(temp);
-        double newDet = det.detkof(temp);
-        System.out.println(newDet + " " + detM);
-        double resultDet = newDet / detM;
-        mHasil.setElement(mHasil, j, 0, resultDet);
-      }
-      for(int i = 0; i<=mHasil.getLastRowIdx(mHasil) ; i++){
-        result.append("x" + (i+1) + " = " + mHasil.getElement(i, 0) + "\n");
-      } 
+    else{
+      result.append("Input invalid.");
     }
   }
 }
